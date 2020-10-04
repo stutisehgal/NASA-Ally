65429019194a8bf919d4aaa25c50ca5851711935
Avi Gautam <61787292+avigautam-329@users.noreply.github.com>
2020-10-04 16:46:34 +0530

    updated

--- NASAAPPS/projectsite/projectsite/settings.py
+++ NASAAPPS/projectsite/projectsite/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'bootstrap3',
+    'bootstrap4',
     'sitepages',
     'allysocials',
 ]
