f0822bf58cf8439133df82ef9e43a6b9ce051d0c
Avi Gautam <61787292+avigautam-329@users.noreply.github.com>
2020-10-25 14:13:24 +0530

    Update settings.py

--- projectsite/settings.py
+++ projectsite/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -134,5 +133,3 @@ AUTH_USER_MODEL = 'sitepages.User'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
-
-django_heroku.settings(locals())
