b063a028cf420e9d6d692d73d92005658f1dc6ba
Avi Gautam <61787292+avigautam-329@users.noreply.github.com>
2020-10-25 13:56:33 +0530

    deleteing

--- NASAAPPS/projectsite/allysocials/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def showallysocials(request):
-    return render(request,'allysocials/index.html')
-
-
-def showabtallysocials(request):
-    return render(request,'allysocials/blog-single.html')
